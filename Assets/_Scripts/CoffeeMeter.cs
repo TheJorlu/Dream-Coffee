47b30056c2148ff1b0861532ee53136017630d17
Jorge Luis De Alba <jorlu934@gmail.com>
2019-01-26 10:39:29 -0600

    cambios de UI

--- Assets/_Scripts/CoffeeMeter.cs
+++ Assets/_Scripts/CoffeeMeter.cs
@@ -7,15 +7,31 @@ using UnityEngine.UI;
 public class CoffeeMeter : MonoBehaviour
 {
     private Image meterFill;
+    private Animation anim;
 
     private void Start()
     {
         meterFill = GetComponent<Image>();
+        anim = GetComponent<Animation>();
     }
 
     [YarnCommand("rateCoffee")]
     public void UpdateMeter(string value)
     {
-        meterFill.fillAmount = float.Parse(value) / 100;
+        StartCoroutine(DisplayMeter(float.Parse(value) / 100));
+    }
+
+    IEnumerator DisplayMeter(float fillValue)
+    {
+        anim.PlayNormal(1);
+
+        do
+        {
+            meterFill.fillAmount = Mathf.Lerp(meterFill.fillAmount, fillValue, Time.deltaTime);
+            yield return 0;
+        }
+        while (System.Math.Abs(meterFill.fillAmount - fillValue) > 0.01f);
+
+        anim.PlayBackwards(1);
     }
 }
